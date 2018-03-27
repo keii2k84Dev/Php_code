09527df9f885f1e31e0d3d3eff62e759acc92187
t-ckimu <t-ckimu@yahoo-corp.jp>
2018-03-27 11:52:18 +0900

    request url by file

--- topic_match.php
+++ topic_match.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/src/SignalHandler.php';
-require_once __DIR__ . '/src/Consumer.php';
+require_once __DIR__ . '/consumer_skeleton/SignalHandler.php';
+require_once __DIR__ . '/consumer_skeleton/Consumer.php';
 require_once __DIR__.'/vendor/autoload.php';
 
 try {
